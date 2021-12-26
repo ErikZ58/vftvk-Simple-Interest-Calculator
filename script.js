71831ac286a32c885cdca4e022f75061d3274dc7
ErikZ58 <96550756+ErikZ58@users.noreply.github.com>
2021-12-26 17:44:09 +0100

    change result id test implemented

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function compute()
 {
-    alert("Test test test")
     var principal = document.getElementById("principal").value;
     if (principal <= 0)
     {
@@ -13,7 +12,8 @@ function compute()
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
 
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
+    document.getElementById("result").innerHTML= "test test test";
+        //"If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
 }
         
 function updateRate() 
